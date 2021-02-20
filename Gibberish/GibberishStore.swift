f080472a5520e7264b99fda706725c815ae7a4ce
Gero Herkenrath <gero@gero-herkenrath.de>
2021-02-20 23:42:10 +0100

    added a store class that allows loading and publishing lists of gibberish

---
+++ Gibberish/GibberishStore.swift
@@ -0,0 +1,27 @@
+//
+//  GibberishStore.swift
+//  Gibberish
+//
+//  Created by Gero Herkenrath on 20.02.21.
+//
+
+import Foundation
+
+class GibberishStore: ObservableObject {
+
+	@Published var items = [GibberishJsonModel]()
+	@Published var isLoading = false
+
+	var gibberishLoader: GibberishLoading = GibberishLoader()
+
+	func loadMore() {
+		isLoading = true
+		gibberishLoader.download { [weak self] response in
+			defer { self?.isLoading = false }
+			guard case .goodResponse(let newItem) = response else {
+				return
+			}
+			self?.items.append(newItem)
+		}
+	}
+}
