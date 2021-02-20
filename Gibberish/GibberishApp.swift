0f4f7e5bd43b59bac9a1e3d2b42641bcb4a9072f
Gero Herkenrath <gero@gero-herkenrath.de>
2021-02-20 17:17:11 +0100

    initial commit

---
+++ Gibberish/GibberishApp.swift
@@ -0,0 +1,17 @@
+//
+//  GibberishApp.swift
+//  Gibberish
+//
+//  Created by Gero Herkenrath on 20.02.21.
+//
+
+import SwiftUI
+
+@main
+struct GibberishApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
